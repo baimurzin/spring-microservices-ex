99d092a2eacddf52944c2fa14d250e4d997805bd
Baimurzin, Vladislav <baimurzin.719@gmail.com>
2019-02-28 17:37:44 +0200

    swagger

--- room-services/src/main/java/com/baimurzin/services/room/roomservices/RoomServicesApplication.java
+++ room-services/src/main/java/com/baimurzin/services/room/roomservices/RoomServicesApplication.java
@@ -2,8 +2,10 @@ package com.baimurzin.services.room.roomservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RoomServicesApplication {
 
     public static void main(String[] args) {
