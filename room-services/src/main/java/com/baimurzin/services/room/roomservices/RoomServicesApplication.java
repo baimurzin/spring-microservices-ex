39e1288fcd158eeb8f1e0a5a2b83863e743c4696
Baimurzin, Vladislav <baimurzin.719@gmail.com>
2019-02-28 20:15:21 +0200

    added eureka client

--- room-services/src/main/java/com/baimurzin/services/room/roomservices/RoomServicesApplication.java
+++ room-services/src/main/java/com/baimurzin/services/room/roomservices/RoomServicesApplication.java
@@ -2,10 +2,12 @@ package com.baimurzin.services.room.roomservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class RoomServicesApplication {
 
     public static void main(String[] args) {
