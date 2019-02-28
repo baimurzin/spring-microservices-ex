01c68019f0886255dc62dc0ac47b13cb6cc3f619
Baimurzin, Vladislav <baimurzin.719@gmail.com>
2019-02-28 22:58:05 +0200

    hystrix added, fallbacks

--- resertvation-business-servicess/src/main/java/com/baimurzin/business/reservation/resertvationbusinessservicess/ResertvationBusinessServicessApplication.java
+++ resertvation-business-servicess/src/main/java/com/baimurzin/business/reservation/resertvationbusinessservicess/ResertvationBusinessServicessApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -15,13 +16,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @EnableSwagger2
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ResertvationBusinessServicessApplication {
-
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ResertvationBusinessServicessApplication.class, args);
