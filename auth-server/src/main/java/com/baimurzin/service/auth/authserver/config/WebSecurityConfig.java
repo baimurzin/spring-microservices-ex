5708956187493fe83dfb1efc0f3ea4699843416c
vladislavbaimurzin <baimurzin.719@gmail.com>
2019-03-07 00:16:13 +0200

    added database security

--- auth-server/src/main/java/com/baimurzin/service/auth/authserver/config/WebSecurityConfig.java
+++ auth-server/src/main/java/com/baimurzin/service/auth/authserver/config/WebSecurityConfig.java
@@ -1,30 +1,38 @@
 package com.baimurzin.service.auth.authserver.config;
 
+import com.baimurzin.service.auth.authserver.security.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final BCryptPasswordEncoder passwordEncoder;
 
-    public WebSecurityConfig(BCryptPasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
+    private final CustomUserDetailsService userDetailsService;
 
-    @Autowired
-    public void globalUserDetails(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("john").password(passwordEncoder.encode("123")).roles("USER").and()
-                .withUser("tom").password(passwordEncoder.encode("111")).roles("ADMIN").and()
-                .withUser("user1").password(passwordEncoder.encode("pass")).roles("USER").and()
-                .withUser("admin").password(passwordEncoder.encode("nimda")).roles("ADMIN");
+//    @Autowired
+//    public void globalUserDetails(final AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("john").password(passwordEncoder.encode("123")).roles("USER").and()
+//                .withUser("tom").password(passwordEncoder.encode("111")).roles("ADMIN").and()
+//                .withUser("user1").password(passwordEncoder.encode("pass")).roles("USER").and()
+//                .withUser("admin").password(passwordEncoder.encode("nimda")).roles("ADMIN");
+//    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
@@ -37,6 +45,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().csrf().disable();
     }
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider
+                = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
 
     @Override
     @Bean
