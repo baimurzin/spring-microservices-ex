b67688213525955891923742ffa206b0e9b88bd0
vladislavbaimurzin <baimurzin.719@gmail.com>
2019-03-06 23:25:20 +0200

    made security works again

--- gateway-zuul/src/main/java/com/bimurzin/gatewayzuul/gatewayzuul/GatewayZuulApplication.java
+++ gateway-zuul/src/main/java/com/bimurzin/gatewayzuul/gatewayzuul/GatewayZuulApplication.java
@@ -3,9 +3,11 @@ package com.bimurzin.gatewayzuul.gatewayzuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableResourceServer
 public class GatewayZuulApplication {
 
     public static void main(String[] args) {
